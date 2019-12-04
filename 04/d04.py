f9993b9c5aeb7a07a70e8456c632caa4f2a6e63a
Bohdan Widla <bohdan@bohdanwidla.pl>
2019-12-04 16:20:46 +0100

    Refactor day 4.

--- 04/d04.py
+++ 04/d04.py
@@ -2,38 +2,19 @@ print("Day 4 of Advent of Code!")
 
 from collections import Counter
 
+def is_valid(d):
+    d = str(d)
+    return sorted(d) == list(d) and len(set(d)) != len(d)
+    
+def is_valid_proper(d):
+    d = str(d)
+    return sorted(d) == list(d) and 2 in Counter(d).values()
+
 lower = 248345
 upper = 746315
 test_digs = range(lower, upper + 1)
 
-def is_valid(d):
-    d = str(d)
-    adj = False
-    for i in range(len(d) - 1):
-        d1, d2 = int(d[i]), int(d[i+1])
-        if d1 > d2:
-            return False, adj
-        if d1 == d2:
-            adj = True
-    return True, adj
-
-def is_valid_proper(d):
-    d = str(d)
-    inc, adj = True, False
-    c = Counter(d)
-    ok = {}
-    for i in range(len(d) - 1):
-        d1, d2 = int(d[i]), int(d[i+1])
-        if d1 > d2:
-            inc = False
-            return inc, adj
-        if d1 == d2:
-            ok[str(d1)] = c[str(d1)]
-            if 2 in ok.values():
-                adj = True
-    return inc, adj
-
-valid = [t for t in test_digs if all(is_valid(t))]
+valid = [t for t in test_digs if is_valid(t)]
 print("Number of valid according to first set of criteria:", len(valid))
-proper_valid = [t for t in test_digs if all(is_valid_proper(t))]
+proper_valid = [t for t in test_digs if is_valid_proper(t)]
 print("Number of valid according to second set of criteria:", len(proper_valid))
